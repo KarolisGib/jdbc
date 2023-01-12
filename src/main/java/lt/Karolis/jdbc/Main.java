af96eb6f595d82d00f12307abf87c5c8fcbeb5be
KarolisGib <karolisgibavicius@yahoo.com>
2023-01-12 17:05:54 +0200

    connected

---
+++ src/main/java/lt/Karolis/jdbc/Main.java
@@ -0,0 +1,23 @@
+package lt.Karolis.jdbc;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Connection c = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/classicmodels",
+                    "root",
+                    "root");
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from customers");
+
+            while (rs.next())
+                System.out.println(rs.getInt(1) +
+                        " | " +
+                        rs.getString(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
